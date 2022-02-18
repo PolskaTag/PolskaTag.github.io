be31a9b1979cf40cc6ac5f43cd89a6d8a7816807
Philip Lapinski <41872747+PolskaTag@users.noreply.github.com>
2022-02-18 17:49:59 -0500

    A bit of work to navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,23 +1,27 @@
 function Navbar() {
   return (
-    <nav className="main-nav">
-      <div className="nav-logo">PL</div>
-      <ul>
-        <li>
-          {" "}
-          <a>Home</a>
-        </li>
-        <li>
-          <a>About Me</a>
-        </li>
-        <li>
-          <a>My Work</a>
-        </li>
-        <li>
-          <a>Contact</a>
-        </li>
-      </ul>
-    </nav>
+    <header className="navbar-header">
+      <nav className="main-nav">
+        <div className="nav-logo">PL</div>
+        <div className="nav-links">
+          <ul>
+            <li>
+              {" "}
+              <a>Home</a>
+            </li>
+            <li>
+              <a>About Me</a>
+            </li>
+            <li>
+              <a>My Work</a>
+            </li>
+            <li>
+              <a>Contact</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </header>
   );
 }
 
