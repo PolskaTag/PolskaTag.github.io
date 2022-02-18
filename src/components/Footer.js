d36a59feef7917e7e84655743b7e470581c7636b
Philip Lapinski <41872747+PolskaTag@users.noreply.github.com>
2022-02-18 17:00:57 -0500

    Css changes to Home and footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,46 @@
+import instaIcon from "../resources/insta-white.png";
+import linkedInIcon from "../resources/linkedIn-white.png";
+import githubIcon from "../resources/github-white.png";
+import twitterIcon from "../resources/twitter-white.png";
+
 function Footer() {
-  return <p>hello World</p>;
+  return (
+    <section className="footer">
+      <div className="plugs">
+        <a
+          className="git-plug"
+          href="https://github.com/PolskaTag"
+          target="_blank"
+        >
+          <img className="" src={githubIcon}></img>
+        </a>
+        <a
+          className="insta-plug"
+          href="https://www.instagram.com/philip.lapinski/"
+          target="_blank"
+        >
+          <img className="" src={instaIcon}></img>
+        </a>
+        <a
+          className="linkedIn-plug"
+          href="https://www.linkedin.com/in/philip-lapinski/"
+          target="_blank"
+        >
+          <img className="" src={linkedInIcon}></img>
+        </a>
+        <a
+          className="twitter-plug"
+          href="https://twitter.com/philip_lapinski"
+          target="_blank"
+        >
+          <img className="" src={twitterIcon}></img>
+        </a>
+      </div>
+      <div className="creds">
+        <span>Designed & Built by Philip Lapinski</span>
+      </div>
+    </section>
+  );
 }
 
 export default Footer;
