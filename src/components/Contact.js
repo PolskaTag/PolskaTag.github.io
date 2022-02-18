ca316c75d1ae76f1d4d66089df16b963c8a0649e
Philip Lapinski <41872747+PolskaTag@users.noreply.github.com>
2022-02-18 14:00:49 -0500

    Updated CSS to Contact.js

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,12 +2,14 @@ function Contact() {
   return (
     <section className="contact">
       <h1>Say Hello!</h1>
-      <h3>
-        I’m looking for new opportunties. If it sounds interesting and you have
+      <h3 className="contact-body">
+        I'm open for new opportunities. If it sounds interesting and you have
         excitement for it, let me hear what it’s about! Whether local or across
         the globe, drop me a line :)
       </h3>
-      <button>The Line</button>
+      <a className="email-link" href="philiplapinski.pl@gmail.com" target="">
+        The Line :)
+      </a>
     </section>
   );
 }
