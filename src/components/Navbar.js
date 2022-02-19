0d98a5a46552b74a81ea9c90ac8b252c9df31c7b
Philip Lapinski <41872747+PolskaTag@users.noreply.github.com>
2022-02-19 17:23:10 -0500

    Mobile Updates to Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,24 +1,46 @@
 function Navbar() {
+  const activateHamburger = (e) => {
+    const nav = document.querySelector(".nav-links");
+    const navLinks = document.querySelectorAll(".nav-links li");
+    nav.classList.toggle("nav-active");
+
+    // animate the links
+    navLinks.forEach((link, index) => {
+      if (link.style.animation) {
+        link.style.animation = "";
+      } else {
+        link.style.animation = `navLinkFade 0.5s ease forwards ${
+          index / 7 + 1
+        }s`;
+      }
+    });
+  };
+
   return (
     <header className="navbar-header">
       <nav className="main-nav">
         <div className="nav-logo">PL</div>
-        <div className="nav-links">
-          <ul>
+        <div className="nav-links-container">
+          <ul className="nav-links">
             <li>
               {" "}
-              <a>Home</a>
+              <a href="#">Home</a>
             </li>
             <li>
-              <a>About Me</a>
+              <a href="#">About Me</a>
             </li>
             <li>
-              <a>My Work</a>
+              <a href="#">My Work</a>
             </li>
             <li>
-              <a>Contact</a>
+              <a href="#">Contact</a>
             </li>
           </ul>
+          <div className="burger" onClick={activateHamburger}>
+            <div className="burger-line1"></div>
+            <div className="burger-line2"></div>
+            <div className="burger-line3"></div>
+          </div>
         </div>
       </nav>
     </header>
