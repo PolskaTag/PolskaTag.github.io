005d7b35c964dbc111acf3991aa055472a215a76
Philip Lapinski <41872747+PolskaTag@users.noreply.github.com>
2022-03-16 21:13:15 -0400

    Changed home to use grid CSS

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,20 +4,23 @@ import resume from "../resources/PhilipLapinskiResume.pdf";
 function Home() {
   return (
     <section className="home" id="home">
-      <div className="philip-img">
+      <div className="home-philip-img">
         <img src={philipImg} alt="Me :)"></img>
       </div>
-      <div className="home-text">
-        <h1 className="home-title">Hi, I'm Philip Lapinski.</h1>
-        <h3>
-          I’m a <b>software engineer</b> who likes to bring complex ideas to
-          life. My current focus is to explore the varying realms of software
-          developement and see which one I can best serve.
-        </h3>
-        <a className="resume" href={resume} target="_blank" rel="noreferrer">
-          Resume
-        </a>
-      </div>
+      <h1 className="home-title">Hi, I'm Philip Lapinski.</h1>
+      <h3 className="home-subtitle">
+        I’m a <b>software engineer</b> who likes to bring complex ideas to life.
+        My current focus is to explore the varying realms of software
+        developement and see which one I can best serve.
+      </h3>
+      <a
+        className="resume home-action"
+        href={resume}
+        target="_blank"
+        rel="noreferrer"
+      >
+        Resume
+      </a>
     </section>
   );
 }
